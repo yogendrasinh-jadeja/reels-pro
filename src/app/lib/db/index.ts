0b638d6bf73a343ff981abb5c11f80f1b0cfe1ec
yogendrasinh jadeja <meyogendrasinh.jadeja@gmail.com>
2025-02-02 07:44:34 +0530

    Fixed typescript issue in database connection

--- src/app/lib/db/index.ts
+++ src/app/lib/db/index.ts
@@ -2,44 +2,43 @@ import mongoose, { Connection } from "mongoose";
 import { config } from "../utils/config.js";
 
 if (!config.mongoDbUri) {
-    throw new Error("please define mongodb uri in env file")
+    throw new Error("Please define MongoDB URI in the env file");
 }
 
 declare global {
-    var mongoose: {
-        conn: Connection | null,
-        promise: Promise<Connection> | null
-    }
+    var mongooseCache: {
+        conn: Connection | null;
+        promise: Promise<Connection> | null;
+    };
 }
 
-let cached = global.mongoose
+let cached = global.mongooseCache;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null }
+    cached = global.mongooseCache = { conn: null, promise: null };
 }
 
-export const connectDB = async () => {
-    if (cached) {
-        return cached.conn
+export const connectDB = async (): Promise<Connection> => {
+    if (cached.conn) {
+        return cached.conn;
     }
 
     if (!cached.promise) {
         const options = {
-            bufferCommand: true,
-            maxPoolSize: 10
-        }
-        cached.promise = await mongoose.connect(`${config.mongoDbUri}/${config.mongoDbDatabaseName}`, options).then(() => {
-            mongoose.connection
-        })
+            bufferCommands: true,
+            maxPoolSize: 10,
+        };
+        cached.promise = mongoose.connect(`${config.mongoDbUri}/${config.mongoDbDatabaseName}`, options).then(
+            (connection) => connection.connection
+        );
     }
 
     try {
-        cached.conn = await cached.promise
+        cached.conn = await cached.promise;
     } catch (error) {
-        cached.promise =null
-        throw new Error("check database file ")
+        cached.promise = null;
+        throw new Error("Check database connection file");
     }
 
-    return cached.conn
-
-}
+    return cached.conn;
+};
